c87374769ec4038e32735f7669cb37d200892913
poojakira <poojakira@users.noreply.github.com>
2026-07-11 03:28:19 +0000

    init: model-privacy-attacks scaffold

---
+++ src/privacy_attacks/model_extraction/__init__.py
@@ -0,0 +1,5 @@
+"""Model extraction attacks: cloning a black-box model from its outputs."""
+
+from privacy_attacks.model_extraction.boundary_stealing import BoundaryStealingAttack
+
+__all__ = ["BoundaryStealingAttack"]
